d17619e432e65e58f68f686746bd74ffd0c1e8a1
Isabel Lee <ijl0322@gmail.com>
2019-03-28 14:47:43 -0400

    Lesson 5

--- TicTacToe/Assets/Scripts/GameController.cs
+++ TicTacToe/Assets/Scripts/GameController.cs
@@ -1,18 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Text[] buttonList;
+
+    void SetGameControllerOnButtons()
     {
-        
+        for (int i = 0; i < buttonList.Length; i++)
+        {
+            buttonList[i].GetComponentInParent<GridSpace>().SetGameControllerReference(this);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void Awake()
     {
-        
+        SetGameControllerOnButtons();
+    }
+
+    public string GetPlayerSide()
+    {
+        return "?";
+    }
+
+    public void EndTurn()
+    {
+        Debug.Log("End turn");
     }
 }
--- TicTacToe/Assets/Scripts/GridSpace.cs
+++ TicTacToe/Assets/Scripts/GridSpace.cs
@@ -7,11 +7,17 @@ public class GridSpace : MonoBehaviour
 {
     public Button button;
     public Text buttonText;
-    public string playerSide;
+    private GameController gameController;
 
     public void SetSpace()
     {
-        buttonText.text = playerSide;
+        buttonText.text = gameController.GetPlayerSide();
         button.interactable = false;
+        gameController.EndTurn();
+    }
+
+    public void SetGameControllerReference(GameController controller)
+    {
+        gameController = controller;
     }
 }
