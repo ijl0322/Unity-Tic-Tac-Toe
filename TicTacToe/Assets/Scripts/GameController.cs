cac0cc7f8f305064723ebef93f972d271522b78a
Isabel Lee <ijl0322@gmail.com>
2019-03-28 14:55:05 -0400

    Lesson 6 1/2

--- TicTacToe/Assets/Scripts/GameController.cs
+++ TicTacToe/Assets/Scripts/GameController.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class GameController : MonoBehaviour
 {
     public Text[] buttonList;
+    private string playerSide;
 
     void SetGameControllerOnButtons()
     {
@@ -18,15 +19,26 @@ public class GameController : MonoBehaviour
     void Awake()
     {
         SetGameControllerOnButtons();
+        playerSide = "X";
     }
 
     public string GetPlayerSide()
     {
-        return "?";
+        return playerSide;
     }
 
     public void EndTurn()
     {
-        Debug.Log("End turn");
+        if (buttonList[0].text == playerSide && buttonList[1].text == playerSide && buttonList[2].text == playerSide) {
+            GameOver();
+        }
+    }
+
+    void GameOver()
+    {
+        for (int i = 0; i < buttonList.Length; i++)
+        {
+            buttonList[i].GetComponentInParent<Button>().interactable = false;
+        }
     }
 }
